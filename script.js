49b5a3e1c012aa5eacf9e43e2d02fff478497097
DarlanAguiar <darlangaguiar@gmail.com>
2021-11-08 19:00:59 -0400

    editando genius

--- script.js
+++ script.js
@@ -23,21 +23,21 @@ if(localStorage.contadorVisitas){
 
 }else{
     localStorage.contadorVisitas = 1;
-    localStorage.nome = "Jogador Indefinido"
+    localStorage.nome = "Jogador"
 }
 
 
 let contador = document.querySelector("[data-contador]");
 
 
-if(localStorage.nome == "Jogador Indefinido"){
+if(localStorage.nome == "Jogador"){
 
     if(localStorage.contadorVisitas == "1"){
         contador.innerHTML = `Olá Jogador, seja bem vindo.`
     }else{
         contador.innerHTML = `Você irá jogar pela <span>${localStorage.contadorVisitas}</span> vez`
     }
-}else if(localStorage.nome != "Jogador Indefinido" && localStorage.nome.length > 0){
+}else{
     if(localStorage.contadorVisitas == "1"){
         contador.innerHTML = `Olá ${localStorage.nome}, seja bem vindo.`
     }else{
@@ -57,11 +57,10 @@ function mostraCampo (){
 
 function salvaJogador (){
     nome = document.querySelector("[data-nome]");
-    if(nome.value == '' || nome.value == "undefined"){
+    if(nome.value == ''){
         alert("você precisa digitar um nome");
         return
     }
-    console.log(nome.value)
     localStorage.nome = nome.value;
     localStorage.contadorVisitas = 0;
     campo.style.display = "none"
